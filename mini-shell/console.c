c633e59f5413fd756fcdd613c5f36f60b8c15e9a
thekarlx <xdeveloping@outlook.com>
2026-02-24 20:37:57 -0500

    Bugfixex of the day

--- mini-shell/console.c
+++ mini-shell/console.c
@@ -3,30 +3,45 @@
 #include <ctype.h>
 #include <stdint.h>
 
-#include <windows.h> // As of feb 24th 2026, shell commands are only available for windows
+#include <windows.h>  // Shell commands (Windows only)
 
 #pragma comment(lib, "Shell32.lib")
 
+int state;
+
 char buff[512];
-char vemo_fname[] = "vemo"; // No .txt so that Windows 11 Notepad can't get there
+char vemo_fname[] = "vemo";
 uint8_t vemo = 0;
 
 FILE *fp;
 
 int main() {
+
+    fp = fopen(vemo_fname, "r");
+    if (fp != NULL) {
+        if (fscanf(fp, "%d", &state) == 1) {
+            vemo = (uint8_t)state;
+        }
+        fclose(fp);
+    }
+
     printf("OpenSource KarlX Project\n");
 
     while (1) {
+
         printf("/> ");
-        fgets(buff, sizeof(buff), stdin);
+
+        if (!fgets(buff, sizeof(buff), stdin))
+            break;
+
         buff[strcspn(buff, "\n")] = 0;
 
         for (int i = 0; buff[i] != '\0'; i++) {
-            buff[i] = tolower(buff[i]);
+            buff[i] = tolower((unsigned char)buff[i]);
         }
 
-        if (vemo == 1) {
-            printf("[VeMo] Processed input: %s\n", buff);
+        if (vemo) {
+            printf("  [VeMo] Processed input: %s\n", buff);
         }
 
         if (strcmp(buff, "exit") == 0) {
@@ -34,10 +49,10 @@ int main() {
             break;
 
         } else if (strcmp(buff, "vemo") == 0) {
+
             printf("Verbose Mode (-s to see status, -t to toggle)\n");
 
         } else if (strcmp(buff, "vemo -s") == 0) {
-            int state;
 
             fp = fopen(vemo_fname, "r");
             if (fp != NULL) {
@@ -46,7 +61,7 @@ int main() {
                 }
                 fclose(fp);
             } else {
-                printf("%s not found.\n", vemo_fname);
+                printf("%s not found. Using default value (0/off).\n", vemo_fname);
             }
 
             switch (vemo) {
@@ -59,6 +74,7 @@ int main() {
             }
 
         } else if (strcmp(buff, "vemo -t") == 0) {
+
             vemo = !vemo;
 
             fp = fopen(vemo_fname, "w");
@@ -67,22 +83,28 @@ int main() {
                 fclose(fp);
             }
 
-            printf("Vemo toggled %s\n", vemo ? "on" : "off");
-        
-            // SHELL COMMANDS (windows only)
-        } else if (strcmp(buff,"notepad") == 0) {
-            ShellExecute(NULL,"open","notepad",NULL,NULL,SW_SHOWNORMAL);
-        } else if (strcmp(buff,"explorer") == 0) {
-            ShellExecute(NULL,"open","explorer",NULL,NULL,SW_SHOWNORMAL);
-        } else if (strcmp(buff,"regedit") == 0) {
-            ShellExecute(NULL,"open","regedit",NULL,NULL,SW_SHOWNORMAL);
-        } else if (strcmp(buff,"control") == 0) {
-            ShellExecute(NULL,"open","control",NULL,NULL,SW_SHOWNORMAL);
-        } else if (strcmp(buff,"taskmgr") == 0) {
-            ShellExecute(NULL,"open","taskmgr",NULL,NULL,SW_SHOWNORMAL);
+        } else if (strcmp(buff, "notepad") == 0) {
+
+            ShellExecute(NULL, "open", "notepad", NULL, NULL, SW_SHOWNORMAL);
+
+        } else if (strcmp(buff, "explorer") == 0) {
+
+            ShellExecute(NULL, "open", "explorer", NULL, NULL, SW_SHOWNORMAL);
+
+        } else if (strcmp(buff, "regedit") == 0) {
+
+            ShellExecute(NULL, "open", "regedit", NULL, NULL, SW_SHOWNORMAL);
+
+        } else if (strcmp(buff, "control") == 0) {
+
+            ShellExecute(NULL, "open", "control", NULL, NULL, SW_SHOWNORMAL);
+
+        } else if (strcmp(buff, "taskmgr") == 0) {
+
+            ShellExecute(NULL, "open", "taskmgr", NULL, NULL, SW_SHOWNORMAL);
+ 
         }
     }
 
     return 0;
-    
 }
